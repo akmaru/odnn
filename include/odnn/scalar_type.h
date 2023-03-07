bcccdfa9c54752acd1d5b714aac8494598f0c074
Akira Maruoka <akmaru0266@gmail.com>
2023-03-07 18:45:38 +0000

    Added scalar_type.cc

--- include/odnn/scalar_type.h
+++ include/odnn/scalar_type.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <odnn/util.h>
+
 #include <cstddef>
 
 #include "error.h"
@@ -8,14 +10,6 @@ namespace odnn {
 
 enum class ScalarType { kFloat32 };
 
-std::size_t byte_of_scalar_type(ScalarType dtype) {
-  switch (dtype) {
-    case ScalarType::kFloat32:
-      return 4Uz;
-  }
-
-  UNREACHABLE();
-  return 0;
-}
+SizeT byte_of_scalar_type(ScalarType dtype);
 
 }  // namespace odnn
---
+++ src/scalar_type.cc
@@ -0,0 +1,18 @@
+#include <odnn/scalar_type.h>
+#include <odnn/util.h>
+
+#include "error.h"
+
+namespace odnn {
+
+SizeT byte_of_scalar_type(ScalarType dtype) {
+  switch (dtype) {
+    case ScalarType::kFloat32:
+      return 4;
+  }
+
+  UNREACHABLE();
+  return 0;
+}
+
+}  // namespace odnn
